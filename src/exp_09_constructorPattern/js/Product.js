76180a2354b7822c2eb7c04df9baf34b0e7ca85b
tfeuerst <feuerstein.rhp@gmail.com>
2017-12-05 10:21:51 +0100

    [CHG] Extend Exercise 09

--- src/exp_09_constructorPattern/js/Product.js
+++ src/exp_09_constructorPattern/js/Product.js
@@ -3,7 +3,16 @@ this.COMPANY.Person = (function () {
     //'use strict';
     // Funktionsausdruck
     var Person = function (fn) {
+
+        //Private
+        var uid = Math.ceil(Math.random() * 1e8);
+
+        // ES3 Property
         this.fullname = fn;
+
+        this.getUid = function () {
+            return uid;
+        };
     };
 
     Person.isPerson = function (p) {
--- src/exp_09_constructorPattern/js/main.js
+++ src/exp_09_constructorPattern/js/main.js
@@ -4,4 +4,5 @@
     var p1 = new COMPANY.Person('Hans Mayer');
     console.log(p1);
     console.log(COMPANY.Person.isPerson(p1));
+    console.log(p1.getUid());
 }());
