309fc564fe1581b15509b8b66edd4611983bc154
tfeuerst <feuerstein.rhp@gmail.com>
2017-12-05 15:04:06 +0100

    [CHG] Exercise 11 update

--- src/exp_11_methods/js/main.js
+++ src/exp_11_methods/js/main.js
@@ -2,8 +2,11 @@
     'use strict';
 
     var p1 = new COMPANY.Person('Hans Mayer');
+    console.log(p1.projects);
+    console.log(COMPANY.Person.isPerson(p1));
 
-    console.log(p1.sayName());
+    console.log(p1.getId());
 
-
+    p1.projects.push(new COMPANY.Project('neuesProjekt'));
+    console.log(p1.projects);
 }());
