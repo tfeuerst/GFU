8a8faf0efa24cff2d97bec8e4a0f873840f19539
tfeuerst <feuerstein.rhp@gmail.com>
2017-12-06 11:23:36 +0100

    [TASK] Project 24

---
+++ src/exp_24_arrowFunction/js/main.js
@@ -0,0 +1,45 @@
+var f1 = function (a) {
+    return a*2;
+};
+
+f1(2);
+
+var f2 = (a) => {
+    return a*2;
+};
+f2(3);
+
+var f3 = a => a*2;
+f3(3);
+
+
+function f5 () {
+    console.log("ok");
+}
+document.addEventListener('click', f5);
+
+
+class Product {
+    constructor() {
+        this.a = 1;
+    }
+
+    doSomething () {
+
+        let f = function () {
+            console.log(this);
+        };
+        f();
+
+        let g = () => {
+            return this.a;
+        };
+        g();
+
+        console.log(this);
+
+    }
+}
+
+let p = new Product();
+p.doSomething();
